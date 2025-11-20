e984a9144e81c27655987affb50d4d58ee343b61
Max Mustermann <max@mustermann.com>
2025-11-20 21:57:57 +0100

    Added treesitter, mini.pairs, a few keybinds and a colorscheme picker

--- init.lua
+++ init.lua
@@ -1,21 +1,82 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.swapfile = false
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.winborder = "rounded"
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.swapfile = false
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.winborder = "rounded"
+vim.o.pumborder = "rounded"
+vim.g.mapleader = " "
+
+vim.keymap.set('n', '<leader>qq', ':q<CR>')
+vim.keymap.set('n', '<leader>hrr', ':update<CR> :so<CR>')
+vim.keymap.set('n', '<leader>fs', ':write<CR>')
+vim.keymap.set('n', '<leader>ff', ':Pick files<CR>')
+vim.keymap.set('n', '<leader>fh', ':Pick help<CR>')
+vim.keymap.set('n', '<leader>fp', 
+  function() 
+    vim.cmd.edit(vim.fn.stdpath("config") .. "/init.lua") 
+  end
+)
 
 vim.pack.add ({
 	{src = 'https://github.com/nvim-mini/mini.pick'},
+	{src = 'https://github.com/nvim-mini/mini.pairs'},
 	{src = 'https://github.com/vague-theme/vague.nvim'},
 	{src = 'https://github.com/mfussenegger/nvim-jdtls'},
+  {src = 'https://github.com/nvim-treesitter/nvim-treesitter'},
 })
 
-require "mini.pick".setup()
+require "mini.pairs".setup()
+require "mini.pick".setup({
+	window = {
+		config = function()
+			local width = math.floor(vim.o.columns * 0.5)
+			local height = 12
+			local row = 3
+			local col = math.floor((vim.o.columns - width) / 2)
+
+			return {
+	 			anchor = 'NW',
+				border = 'rounded',
+				width = width,
+				height = height,
+				row = row,
+				col = col,
+			}
+		end,
+}}) 
+
+-- Colorscheme picker
+local function list_colorschemes() 
+  return vim.fn.getcompletion('', 'color')
+end
+
+local function pick_colorschemes()
+  MiniPick.start({
+    source = {
+      items = list_colorschemes,
+      name = "Colorschemes",
+    },
+    choose = function(item)
+      vim.cmd.colorscheme(item)
+    end,
+  })
+end
+
+vim.keymap.set('n', '<leader>ht', pick_colorschemes)
+
+
 
 vim.lsp.enable({ "jdtls" })
 
+require "nvim-treesitter.configs".setup({
+  ensure_installed = { 'java', 'xml', },
 
+  highlight = { enable = true },
+
+  indent = { enable = false },
+}) 
 
 vim.api.nvim_create_autocmd('LspAttach', {
 	callback = function(args)
